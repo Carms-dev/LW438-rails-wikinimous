f46eb08fa2f85b0a3e34abac1b0a5c0f40809a6c
Carms Ng <carmenhyng89@gmail.com>
2020-08-05 15:09:05 -0400

    modal hookup for create and update

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :update, :destroy]
+
+    def index
+        @articles = Article.all
+        @new_article = Article.new
+    end
+    
+    def show
+        @new_article = Article.new
+    end
+    
+    def create
+        @article = Article.create(article_params)
+        redirect_to article_path(@article)
+    end
+
+    def update
+        @article.update(article_params)
+        redirect_to article_path(@article)
+    end
+    
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+end
